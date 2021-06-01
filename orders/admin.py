a16b550f86b1fb39f903c953d5e40d72d65c787a
snaybanban <36524932+snaybanban@users.noreply.github.com>
2021-06-01 15:23:39 -0600

    Add files via upload

--- orders/admin.py
+++ orders/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from .models import Post, Profile, Relationship, Item_Category, Item, Topping
+from .models import Post, Profile, Item_Category, Item, Topping, Cart
 
 # Register your models here.
 admin.site.register(Profile)
 admin.site.register(Post)
-admin.site.register(Relationship)
 admin.site.register(Item_Category)
 admin.site.register(Item)
 admin.site.register(Topping)
+admin.site.register(Cart)
